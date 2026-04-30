5b2eb3003d659f8f59b0b7e162e9864832a97b7b
Nicolas Stepien <stepien.nicolas@gmail.com>
2026-04-30 18:42:53 +0100

    tsgolint issue repro

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+
   fmt: {
     singleQuote: true,
   },
